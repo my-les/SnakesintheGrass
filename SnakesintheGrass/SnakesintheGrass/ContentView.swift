9609462636eaa1a2c9ab553871a893f55131a861
Myles <mcutchember@gmail.com>
2024-09-29 19:20:41 -0700

    first commit
    lets goooo

---
+++ SnakesintheGrass/SnakesintheGrass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnakesintheGrass
+//
+//  Created by myle$ on 9/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnakesintheGrass/SnakesintheGrass/SnakesintheGrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakesintheGrassApp.swift
+//  SnakesintheGrass
+//
+//  Created by myle$ on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SnakesintheGrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
